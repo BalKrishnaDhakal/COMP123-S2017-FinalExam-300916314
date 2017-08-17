ec9e88f76a7d33a4c4cfc3aa0ecc5121c0794c38
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-08-17 12:58:04 -0400

    SplashForm Timer Tick Method is Created

--- COMP123-S2017-FinalExam-300916314/SplashForm.cs
+++ COMP123-S2017-FinalExam-300916314/SplashForm.cs
@@ -11,8 +11,8 @@ using System.Windows.Forms;
  * Date: August 17, 2017
  * StudentID: 300916314
  * Description: Final Test Project
- * Version: 0.1 SplashForm Class Is Created
- * */
+ * Version: 0.2 SplashForm Timer Tick Method is Created
+ */
 namespace COMP123_S2017_FinalExam_300916314
 {
     public partial class SplashForm : Form
@@ -20,6 +20,16 @@ namespace COMP123_S2017_FinalExam_300916314
         public SplashForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the SplashForm Timer Tick Methods
+        /// This enables the Timer property
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            SplashFormTimer.Enabled = false;
         }
     }
 }
