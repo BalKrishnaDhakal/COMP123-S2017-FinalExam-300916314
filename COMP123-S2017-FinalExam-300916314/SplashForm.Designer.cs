d0c2fe24c53f2082f2825aab9380cbf8beb70e2a
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-08-17 12:53:26 -0400

    SplashForm Class is Created

--- COMP123-S2017-FinalExam-300916314/SplashForm.Designer.cs
+++ COMP123-S2017-FinalExam-300916314/SplashForm.Designer.cs
@@ -29,10 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.SplashFormTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SplashFormTimer
+            // 
+            this.SplashFormTimer.Enabled = true;
+            this.SplashFormTimer.Interval = 3000;
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer SplashFormTimer;
     }
 }
--- COMP123-S2017-FinalExam-300916314/SplashForm.cs
+++ COMP123-S2017-FinalExam-300916314/SplashForm.cs
@@ -7,7 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*Name: Bal Krishna Dhakal
+ * Date: August 17, 2017
+ * StudentID: 300916314
+ * Description: Final Test Project
+ * Version: 0.1 SplashForm Class Is Created
+ * */
 namespace COMP123_S2017_FinalExam_300916314
 {
     public partial class SplashForm : Form
